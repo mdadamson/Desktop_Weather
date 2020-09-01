37ed669a54d12d114df2d9446525b15de8efae1d
Kolger H <Kolger27@gmail.com>
2020-09-01 18:58:14 -0400

    Test push

--- src/DesktopWeather/DemoCommit.java
+++ src/DesktopWeather/DemoCommit.java
@@ -3,5 +3,6 @@ package DesktopWeather;
 public class DemoCommit {
     public static void main(String[] args) {
         System.out.println("Hello, Group One! - First Commit");
+        System.out.println("I want you to delete this!");
     }
 }
