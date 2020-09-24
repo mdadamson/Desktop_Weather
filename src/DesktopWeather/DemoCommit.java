2b554b58a390152b3a58c5d025253b88fac3eaa2
Michael Adamson <mdadamson21@gmail.com>
2020-09-23 21:18:09 -0400

    Update and rename DemoCommit.java to WeatherApplication.java

--- src/DesktopWeather/DemoCommit.java
+++
@@ -1,8 +0,0 @@
-package DesktopWeather;
-
-public class DemoCommit {
-    public static void main(String[] args) {
-        System.out.println("Hello, Group One! - First Commit");
-        // deleted!
-    }
-}
---
+++ src/DesktopWeather/WeatherApplication.java
@@ -0,0 +1,7 @@
+package DesktopWeather;
+
+public class WeatherApplication {
+    public static void main(String[] args) {
+        
+    }
+}
