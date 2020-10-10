5d2be1a56ca3446dd9aecec6d584c4a9683a3916
Kolger H <Kolger27@gmail.com>
2020-10-09 23:01:52 -0400

    Input data for GUI and icon method

--- src/DesktopWeather/WeatherInput.java
+++ src/DesktopWeather/WeatherInput.java
@@ -11,7 +11,6 @@ public class WeatherInput implements EventHandler<ActionEvent> {
     private WeatherAPI weatherAPI = new WeatherAPI();
     private WeatherTimeDate timeDate = new WeatherTimeDate();
     private WeatherGUI gui = new WeatherGUI();
-    private boolean test; // Testing variable
 
     @Override
     public void handle(ActionEvent event){
@@ -31,39 +30,37 @@ public class WeatherInput implements EventHandler<ActionEvent> {
             gui.dialogBox();
         }
 
-        //calls the methods in WeatherAPI and then displays them on the GUI
+        /*
+         * Sends string value for current, day one,
+         * and day two weather values to the GUI.
+         * Calls weather name method and sends to weatherType
+         * method to send over correct file type for icon
+         */
         try {
-            gui.setWeatherFirst("Current City Name: " + weatherAPI.getCityName() +
-                    "\nCurrent Temperature: " + weatherAPI.getTemperature()+ "\u00B0" +
-                    "\nCurrent Humidity: " + weatherAPI.getHumidity()+"\u0025");
+            gui.setWeatherFirst( "Temperature: " + weatherAPI.getTemperature()+ "\u00B0" +
+                    "\nPrecipitation: " + weatherAPI.getPrecipChanceToday()+ "\u0025" +
+                    "\nHumidity: " + weatherAPI.getHumidity()+ "\u0025" +
+                    "\nPressure: " + weatherAPI.getPressure()+ " mbar" +
+                    "\nWeather Type: " + weatherAPI.getWeatherName());
+            gui.setDayOneImage(weatherType(weatherAPI.getWeatherName()));
 
-            gui.setWeatherSecond("Current City Name: " + weatherAPI.getCityName() +
-                    "\nCurrent Temperature: " + weatherAPI.getTemperature()+ "\u00B0" +
-                    "\nCurrent Humidity: " + weatherAPI.getHumidity()+"\u0025");
+            gui.setWeatherSecond( "Temperature Max: " + weatherAPI.getMaxTempTodayPlusOne()+ "\u00B0" +
+                    "\nTemperature Min: " + weatherAPI.getMinTempTodayPlusOne()+ "\u00B0" +
+                    "\nPrecipitation: " + weatherAPI.getPrecipChanceTodayPlusOne()+ "\u0025" +
+                    "\nHumidity: " + weatherAPI.getHumidityTodayPlusOne()+ "\u0025" +
+                    "\nPressure: " + weatherAPI.getPressureTodayPlusOne()+ " mbar" +
+                    "\nWeather Type: " + weatherAPI.getWeatherNameTodayPlusOne());
+            gui.setDayTwoImage(weatherType(weatherAPI.getWeatherNameTodayPlusOne()));
 
-            gui.setWeatherThird("Current City Name: " + weatherAPI.getCityName() +
-                    "\nCurrent Temperature: " + weatherAPI.getTemperature()+ "\u00B0" +
-                    "\nCurrent Humidity: " + weatherAPI.getHumidity()+"\u0025");
+            gui.setWeatherThird( "Temperature Max: " + weatherAPI.getMaxTempTodayPlusTwo()+ "\u00B0" +
+                    "\nTemperature Min: " + weatherAPI.getMinTempTodayPlusTwo()+ "\u00B0" +
+                    "\nPrecipitation: " + weatherAPI.getPrecipChanceTodayPlusTwo()+ "\u0025" +
+                    "\nHumidity: " + weatherAPI.getHumidityTodayPlusTwo()+ "\u0025" +
+                    "\nPressure: " + weatherAPI.getPressureTodayPlusTwo()+ " mbar" +
+                    "\nWeather Type: " + weatherAPI.getWeatherNameTodayPlusTwo());
+            gui.setDayThreeImage(weatherType(weatherAPI.getWeatherNameTodayPlusTwo()));
 
-            gui.setLabelInput(weatherAPI.getCityName() + ", CT"); // can remove CT later - for testing purposes
-
-            // Testing framework will be removed once data is being passed
-            if (test) {
-                gui.setDayOneImage("file:weather_icons\\icon_snowing.png");
-                gui.setDayTwoImage("file:weather_icons\\icon_drip.png");
-                gui.setDayThreeImage("file:weather_icons\\icon_windy.png");
-                test = false;
-            }
-            else {
-                gui.setDayOneImage("file:weather_icons\\icon_cloudy.png");
-                gui.setDayTwoImage("file:weather_icons\\icon_moon.png");
-                gui.setDayThreeImage("file:weather_icons\\icon_raining.png");
-                test = true;
-            }
-
-
-            System.out.println("Current City Name: " + weatherAPI.getCityName()); // can delete these printouts later, used for testing purposes
-            System.out.println("Current Temperature: " + weatherAPI.getTemperature() + "\n");
+            gui.setLabelInput(weatherAPI.getCityName());
         }
         //invalid inputs throw null pointer exceptions, so when caught, the program displays an error box.
         catch (NullPointerException e){
@@ -71,5 +68,39 @@ public class WeatherInput implements EventHandler<ActionEvent> {
             gui.dialogBox();
             gui.setZipInput();
         }
+    }
+    /*
+     * Takes input string value of weather type
+     * matches the weather type to icon list
+     * returns value of correct icon file
+     */
+    private String weatherType (String value){
+        String weatherType;
+        switch (value) {
+            case "Thunderstorm":
+                weatherType = "file:weather_icons\\icon_lightning_cloudy.png";
+                break;
+            case "Drizzle":
+                weatherType = "file:weather_icons\\icon_drip.png";
+                break;
+            case "Rain":
+                weatherType = "file:weather_icons\\icon_raining.png";
+                break;
+            case "Snow":
+                weatherType = "file:weather_icons\\icon_snowing.png";
+                break;
+            case "Mist":
+                weatherType = "file:weather_icons\\icon_foggy_cloud.png";
+                break;
+            case "Clear Sky":
+                weatherType = "file:weather_icons\\icon_sunny.png";
+                break;
+            case "Cloudy":
+                weatherType = "file:weather_icons\\icon_cloudy.png";
+                break;
+            default:
+                weatherType = "file:weather_icons\\icon_windy.png";
+        }
+        return weatherType;
     }
 }
