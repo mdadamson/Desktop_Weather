1e4c27974cfde99ffdeca4074d6817df18809826
Robert Dickens <hondafan2001@bellsouth.net>
2020-10-10 20:43:29 -0400

    Removed first internet check at startup to prevent multiple internet checks

--- src/DesktopWeather/DesktopWeather.java
+++ src/DesktopWeather/DesktopWeather.java
@@ -1,40 +1,7 @@
 package DesktopWeather;
 
-import javafx.application.Platform;
-
 public class DesktopWeather extends WeatherGUI {
     public static void main(String[] args) {
-        // Variables
-        boolean noConn = false;
-        // Instantiates WeatherAPI
-        WeatherAPI weatherAPI = new WeatherAPI();
-        /**
-         * Try/Catch calls network check method
-         * if method creates no issues there would be
-         * no exception and JavaFX GUI will launch correctly
-         * If the method encounters a exception it will throw the
-         * exception and it will trigger a dialog box letting the user know.
-         */
-        try {
-            Platform.runLater(() -> {
-                    try {
-                        intAlertBox();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            );
-            weatherAPI.checkNetworkConnection();
-            noConn = true;
-        }
-        catch (NetworkConnectionException e){
-            Platform.runLater(() -> {
-                    intCheckBox();
-                }
-            );
-        }
-        // Once internet connection is established the GUI launches
-        if (noConn)
             launch(args);
     }
 }
